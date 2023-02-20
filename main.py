7d1ca36795d03cda97563c4f837bb5d1069367bb
Connor Jacob <116337944+connornjacob@users.noreply.github.com>
2023-02-20 15:32:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-vowels = ['a', 'e', 'i', 'o', 'u', 'y']
+vowels = ['a', 'e', 'i', 'o', 'u']
+point = 0
+y = input('Would you like to count duplicate vowels? Y/N: ').upper().strip()
 
-x = input('please write a sentence:').lower().strip()
-score = 0
+if y == 'Y':
+    x = input('please write a sentence:').lower().strip()
+    for vowel in vowels:
+        point += x.count(vowel)
+
+    print(point)
+
+elif y == 'N':
+    
+    x = input('Please enter your sentence: ').lower()
+    for vowel in vowels:
+        if vowel in x:
+            point += 1 
 
 
-for vowels in vowels:
-    score += x.count(vowels)
-
-
-print(score)
+    print(point)
+    
+else: 
+    print('Please input valid entry')
+    
